e7eade946f4829b6edf1d0b43e3d04897be37c1c
Bonaventurearmaah <bonaventurearmaah02@gmail.com>
2023-02-21 08:55:06 -0800

    print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
+void print_alphabet_x10(void)
+{
+	int counter;
+	int letter;
+
+	for (counter = 0; counter <= 10; counter++)
+	{
+		for (letter = 0; letter < 26; letter++)
+		{
+			_putchar(letter + 97);
+		}
+		_putchar('\n');
+	}
+}
